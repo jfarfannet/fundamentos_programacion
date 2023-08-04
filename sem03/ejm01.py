98ac5dbff38b1309ecb473c63c53e926cd3ac2ae
Jaime Farfan <jfarfannet@gmail.com>
2023-08-04 14:43:12 -0500

    primer commit

---
+++ sem03/ejm01.py
@@ -0,0 +1,12 @@
+nota=10
+print(type(nota))
+nota=10.5
+print(type(nota))
+nota="jaime"
+print(type(nota))
+nota='j'
+print(type(nota))
+nota=True
+print(type(nota))
+nota=12
+
---
+++ sem03/operadores.py
@@ -0,0 +1,24 @@
+a= 10+4
+print(a)
+b=2**3
+print(b)
+c= 10/3
+print(c)
+d= 10//3
+print(d)
+nota=0
+if nota == 13:
+    print("aprobado")
+a=10
+# a=a+1
+# a+=1
+a//=3
+print(a)
+
+nota1=10
+nota2=9
+if nota1 >= 0 and nota1 <=20:
+    print("OK")
+
+res=20%6
+print(res)
